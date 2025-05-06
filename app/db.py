dcf46797b1d1a311f784e8288e8b2202b9d88488
nahuel <nahuelmath@gmail.com>
2025-05-06 16:51:18 -0400

    add dp configs

---
+++ app/db.py
@@ -0,0 +1,9 @@
+from sqlalchemy.orm import Session
+
+DB_NAME = "tarea2"
+DB_USERNAME = "cc5002"
+DB_PASSWORD = "programacionweb"
+DB_HOST = "localhost"
+DB_PORT = 3306
+
+DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
