b1dec35daa643978bc6c2e122fd68ca1f2c30339
Nahuel <nahuelmath@gmail.com>
2025-05-07 00:27:53 -0400

    method: retrieve fotos by actividad id

--- app/models/foto.py
+++ app/models/foto.py
@@ -1,8 +1,12 @@
+from __future__ import annotations
+
 from app.models.base import Base
 
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, Session
 
 from sqlalchemy import String, ForeignKey
+
+from typing import Optional, List
 
 
 class Foto(Base):
@@ -12,3 +16,7 @@ class Foto(Base):
     ruta_archivo: Mapped[str] = mapped_column(String(300))
     nombre_archivo: Mapped[str] = mapped_column(String(300))
     actividad_id: Mapped[int] = mapped_column(ForeignKey("actividad.id"))
+
+    @staticmethod
+    def get_fotos_by_actividad_id(db: Session, actividad_id) -> List[Optional[Foto]]:
+        return db.query(Foto).filter_by(actividad_id=actividad_id).all()
