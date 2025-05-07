206d48b87a6f93f7f3023f0589682627a722d439
Nahuel <nahuelmath@gmail.com>
2025-05-07 00:28:24 -0400

    feat: parsing data to send in home template html

---
+++ app/utilities/parsing.py
@@ -0,0 +1,39 @@
+from sqlalchemy.orm import Session
+
+from app.models.actividad import Actividad
+from app.models.actividad_tema import ActividadTema
+from app.models.comuna import Comuna
+from app.models.foto import Foto
+
+from typing import List, Dict, Any
+
+
+def parse_actividades_for_home_page(db: Session, actividades: List[Actividad]) -> List[Dict[str, Any]]:
+    """Parsea una lista de actividades para generar una lista de diccionarios con la informacion de la actividad
+     y llaves para el nombre de la comuna, tema y foto de la actividad requeridos por la tabla 
+     de la pagina home
+
+    Args:
+        actividades (List[Actividad]): Lista de actividades a parsear              
+
+    Returns:
+        List[Dict[str, Any]]: Lista de diccionarios con la informacion de la actividad
+    """
+
+    data_dict = []
+
+    for actividad in actividades:
+        comuna = Comuna.get_comuna_by_id(db, actividad.comuna_id)
+        tema = ActividadTema.get_actividad_tema_by_actividad_id(
+            db, actividad.id)
+        fotos = Foto.get_fotos_by_actividad_id(db, actividad.id)
+        data_dict.append({
+            "inicio": actividad.dia_hora_inicio,
+            "termino": actividad.dia_hora_termino,
+            "comuna": comuna.nombre,
+            "sector": actividad.sector,
+            "tema": tema.tema,
+            "foto": fotos[1].ruta_archivo
+        })
+
+    return data_dict
