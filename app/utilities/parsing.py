985c52e35b65baa58bdbdbf9e504339f4299d173
Nahuel <nahuelmath@gmail.com>
2025-05-08 22:36:43 -0400

    fix: passing foto ulr

--- app/utilities/parsing.py
+++ app/utilities/parsing.py
@@ -5,6 +5,8 @@ from app.models.actividad_tema import ActividadTema
 from app.models.comuna import Comuna
 from app.models.foto import Foto
 
+from flask import url_for
+
 from typing import List, Dict, Any
 
 
@@ -26,14 +28,20 @@ def parse_actividades_for_home_page(db: Session, actividades: List[Actividad]) -
         comuna = Comuna.get_comuna_by_id(db, actividad.comuna_id)
         tema = ActividadTema.get_actividad_tema_by_actividad_id(
             db, actividad.id)
+
         fotos = Foto.get_fotos_by_actividad_id(db, actividad.id)
+        first_foto_filename = ""
+        if len(fotos) > 0:
+            first_foto = fotos[0]
+            first_foto_filename = first_foto.nombre_archivo
+
         data_dict.append({
             "inicio": actividad.dia_hora_inicio,
             "termino": actividad.dia_hora_termino,
             "comuna": comuna.nombre,
             "sector": actividad.sector,
             "tema": tema.tema.value,
-            # "foto": fotos[1].ruta_archivo
+            "url_foto": url_for("static", filename=f"img/{first_foto_filename}")
         })
 
     return data_dict
