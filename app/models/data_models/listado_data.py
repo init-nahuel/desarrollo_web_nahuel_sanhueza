8b39042a95a5089cc6a910a8499152dd3702f935
Nahuel <nahuelmath@gmail.com>
2025-05-08 23:35:09 -0400

    add: dataclasses for modelling data sended to fe

---
+++ app/models/data_models/listado_data.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+from datetime import datetime
+
+from typing import Optional
+
+from app.models.data_models.base_data_model import BaseDataModel
+
+
+@dataclass
+class ListadoData(BaseDataModel):
+    inicio: Optional[datetime] = None
+    termino: Optional[datetime] = None
+    comuna: str = ""
+    sector: str = ""
+    tema: str = ""
+    nombre_organizador: str = ""
+    total_fotos: int = 0
