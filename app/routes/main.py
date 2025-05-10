0af0e95ecca9899cbb0fd5488f937f44a1dffc59
Nahuel <nahuelmath@gmail.com>
2025-05-09 23:38:05 -0400

    refactor: method applied to unique object many objects via map method

--- app/routes/main.py
+++ app/routes/main.py
@@ -4,7 +4,7 @@ from app.db import get_db_session
 
 from app.models.actividad import Actividad
 
-from app.utilities.parsing import parse_actividades_for_home_page, parse_actividades_for_listado_page
+from app.utilities.parsing import parse_actividades_for_home_page, parse_actividad_to_listado_data
 
 main_routes = Blueprint("main", __name__)
 
@@ -29,8 +29,17 @@ def listado_actividades():
     page = request.args.get("page", 1)
     with next(get_db_session()) as db:
         actividades = Actividad.get_actividades_paginated(db, page)
-        actividades_data = parse_actividades_for_listado_page(db, actividades)
+        actividades_data = list(
+            map(lambda a: parse_actividad_to_listado_data(db, a), actividades))
     return render_template("listado-actividades.html", actividades_data=actividades_data)
+
+# foto, actividad,comuna, actividad_tema
+
+
+@main_routes.get("/detalle-actividad/<int:actividad_id>")
+def detalle_actividad(actividad_id: int):
+
+    return render_template("detalle-actividad.html")
 
 
 @main_routes.get("/estadisticas")
--- app/utilities/parsing.py
+++ app/utilities/parsing.py
@@ -49,25 +49,23 @@ def parse_actividades_for_home_page(db: Session, actividades: List[Actividad]) -
     return data_dict
 
 
-def parse_actividades_for_listado_page(db: Session, actividades: List[Actividad]) -> List[ListadoData]:
-    """Parsea una lista de actividades para obtener los atributos requeridos que se muestran
-    en la pagina listado de actividades
+def parse_actividad_to_listado_data(db: Session, actividad: Actividad) -> ListadoData:
+    """Parsea una lista de actividades para generar un objeto de tipo ListadoData, esta clase
+    usualmente se utiliza para enviar datos a la pagina listado de actividad los campos de la bd
+    que se requieren, tambien se utiliza para enviar los datos a detalle-actividad
 
     Args:
-        actividades (List[Actividad]): Lista de actividades a parsear
+        actividad (Actividad): Actividad a parsear
 
     Returns:
-        List[ListadoData]: Lista de diccionarios con la informacion de la actividad
+        ListadoData: Instancia ListadoData con la informacion de la actividad
     """
 
-    data = []
+    comuna = Comuna.get_comuna_by_id(db, actividad.comuna_id)
+    actividad_tema: ActividadTema = ActividadTema.get_actividad_tema_by_actividad_id(
+        db, actividad.id)
+    fotos = Foto.get_fotos_by_actividad_id(db, actividad.id)
+    listado_data = ListadoData(inicio=actividad.dia_hora_inicio, termino=actividad.dia_hora_termino, comuna=comuna.nombre, sector=actividad.sector,
+                               tema=actividad_tema.tema.value, nombre_organizador=actividad.nombre, total_fotos=len(fotos), url=url_for("main.detalle_actividad", actividad_id=actividad.id))
 
-    for actividad in actividades:
-        comuna = Comuna.get_comuna_by_id(db, actividad.comuna_id)
-        actividad_tema: ActividadTema = ActividadTema.get_actividad_tema_by_actividad_id(
-            db, actividad.id)
-        fotos = Foto.get_fotos_by_actividad_id(db, actividad.id)
-        data.append(ListadoData(inicio=actividad.dia_hora_inicio, termino=actividad.dia_hora_termino, comuna=comuna.nombre, sector=actividad.sector,
-                    tema=actividad_tema.tema.value, nombre_organizador=actividad.nombre, total_fotos=len(fotos), url=url_for("main.detalle_actividad", actividad_id=actividad.id)))
-
-    return data
+    return listado_data
