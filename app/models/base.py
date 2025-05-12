3007d9b206220987ce3ac8c318839f4727505f09
Nahuel <nahuelmath@gmail.com>
2025-05-12 12:02:08 -0400

    feat: get all entities method on base clase

--- app/models/base.py
+++ app/models/base.py
@@ -1,10 +1,16 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Session
 
 from sqlalchemy.inspection import inspect
 
-from typing import Dict, Any
+from typing import Dict, Any, List, Type, TypeVar
+
+T = TypeVar("T", bound="Base")
 
 
 class Base(DeclarativeBase):
     def to_dict(self) -> Dict[str, Any]:
         return {column.key: getattr(self, column.key) for column in inspect(self).mapper.column_attrs}
+
+    @classmethod
+    def get_all_entities(cls: Type[T], db: Session) -> List[T]:
+        return db.query(cls).all()
