e4823c196a77e4d13e21bc3df62ba78d6517d5c8
Nahuel <nahuelmath@gmail.com>
2025-05-16 14:17:38 -0400

    fix: negation for validation

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -10,25 +10,25 @@ def validate_actividad_data(region: str, comuna: str, sector: str, nombre_organi
     """Valida la informacion enviada en la peticion a la hora de crear una actividad
     """
 
-    if validations.validate_region(region):
+    if not validations.validate_region(region):
         return (False, "Valor de region invalido")
-    if validations.validate_comuna(comuna):
+    if not validations.validate_comuna(comuna):
         return (False, "Valor de comuna invalido")
-    if validations.validate_sector(sector):
+    if not validations.validate_sector(sector):
         return (False, "Valor de sector invalido")
-    if validations.validate_nombre(nombre_organizador):
+    if not validations.validate_nombre(nombre_organizador):
         return (False, "Valor de nombre organizador invalido")
-    if validations.validate_email(email_organizador):
+    if not validations.validate_email(email_organizador):
         return (False, "Valor de email organizador invalido")
-    if validations.validate_phone_number(telefono_organizador):
+    if not validations.validate_phone_number(telefono_organizador):
         return (False, "Valor de telefono organizador invalido")
-    if validations.validate_medio_contacto(medio_contacto, identificador):
+    if not validations.validate_medio_contacto(medio_contacto, identificador):
         return (False, "Valor de medio contacto invalido")
-    if validations.validate_fecha_inicio(dia_hora_inicio):
+    if not validations.validate_fecha_inicio(dia_hora_inicio):
         return (False, "Valor de fecha inicio invalido")
-    if validations.validate_fecha_termino(dia_hora_termino):
+    if not validations.validate_fecha_termino(dia_hora_termino):
         return (False, "Valor de fecha termino invalido")
-    if validations.validate_tema(tema, tema_otro):
+    if not validations.validate_tema(tema, tema_otro):
         return (False, "Valor de tema de actividad invalido")
     if not validations.validate_fotos(fotos):
         return (False, "Fotos invalidas")
