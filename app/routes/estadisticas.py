4371812c0c10082187fb596c71b11775765c20fd
nahuel <nahuelmath@gmail.com>
2025-06-10 17:15:06 -0400

    feat: endpoint estadisticas por fecha

--- app/routes/estadisticas.py
+++ app/routes/estadisticas.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify
+import itertools
 
 from app.db import get_db_session
 from app.models import Actividad
@@ -13,4 +14,11 @@ def get_cant_actividades_por_dia():
     actividades = []
     with next(get_db_session()) as db:
         actividades = Actividad.get_all_entities(db)
-    # TODO: Usar itertools para agrupar por fecha inicio dia y retornar como json
+    sorted_actividades = sorted(
+        actividades, key=lambda a: a.dia_hora_inicio.date())
+
+    data = []
+    for k, g in itertools.groupby(sorted_actividades, key=lambda a: a.dia_hora_inicio.date()):
+        val = {k.strftime("%d %b %Y"): len(list(g))}
+        data.append(val)
+    return jsonify(data)
